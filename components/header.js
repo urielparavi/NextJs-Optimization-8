d670b23b26f4b327640780fb17b1fb8e3c9daac3
Your Name <urielparavi75@gmail.com>
2025-06-10 10:19:39 +0300

    197. Controlling the Image Size

--- components/header.js
+++ components/header.js
@@ -4,11 +4,25 @@ import Link from 'next/link';
 
 export default function Header() {
   console.log(logo);
-  
+
   return (
     <header id="main-header">
       <Link href="/">
-        <Image src={logo} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          // sizes="10vw"
+          alt="Mobile phone with posts feed on it"
+          /* 
+    The 'sizes' attribute tells the browser that this image
+    will take up approximately 10% of the viewport width (10vw).
+    This allows the browser to select the most appropriate
+    image size from the 'srcset' for different screen widths,
+    optimizing loading performance and ensuring the image
+    looks good on all devices.
+  */
+        />
       </Link>
       <nav>
         <ul>
@@ -16,7 +30,9 @@ export default function Header() {
             <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
