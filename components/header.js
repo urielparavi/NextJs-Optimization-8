a8eae2bba18536a4fe9617621ce10da74d07f38f
Your Name <urielparavi75@gmail.com>
2025-06-10 10:43:56 +0300

    198. Working with Priority Images & More Settings

--- components/header.js
+++ components/header.js
@@ -12,16 +12,23 @@ export default function Header() {
           src={logo}
           width={100}
           height={100}
+          priority
+          /*
+          The 'priority' attribute disables lazy loading for this image,
+          causing it to load immediately. This is useful for important
+          images that are always visible above the fold — like logos or
+          hero images — where fast loading improves perceived performance.
+        */
           // sizes="10vw"
           alt="Mobile phone with posts feed on it"
           /* 
-    The 'sizes' attribute tells the browser that this image
-    will take up approximately 10% of the viewport width (10vw).
-    This allows the browser to select the most appropriate
-    image size from the 'srcset' for different screen widths,
-    optimizing loading performance and ensuring the image
-    looks good on all devices.
-  */
+        The 'sizes' attribute tells the browser that this image
+        will take up approximately 10% of the viewport width (10vw).
+        This allows the browser to select the most appropriate
+        image size from the 'srcset' for different screen widths,
+        optimizing loading performance and ensuring the image
+        looks good on all devices.
+      */
         />
       </Link>
       <nav>
